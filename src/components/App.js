d5522f773819a147433ea6932bcea0ec0257ee90
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-12-19 12:00:10 +0300

    all

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,10 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <header>Header Content</header>
+  <aside>Aside Content</aside>
+  <main>Main Content</main>
+  
     </div>
   );
 }
